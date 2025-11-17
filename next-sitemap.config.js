f12ed0271d803be6d0dbd3628fa24fe21c2d024b
Bryan Barangan <barangantrip@gmail.com>
2025-11-17 10:31:47 +0800

    one sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,7 @@ module.exports = {
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,
+  generateIndexSitemap: false, // Generate a single sitemap.xml instead of index
   exclude: [
     '/api/*', // Exclude API routes
     '/server-sitemap.xml', // Exclude server sitemap
